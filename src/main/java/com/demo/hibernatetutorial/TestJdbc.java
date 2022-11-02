31fd98b969f923d081688cfd95cbfb8cb4e3f873
alihmzyv <alihmzyv@gmail.com>
2022-11-02 18:12:09 +0400

    Initial commit, checked connection to db

---
+++ src/main/java/com/demo/hibernatetutorial/TestJdbc.java
@@ -0,0 +1,20 @@
+package com.demo.hibernatetutorial;
+
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJdbc {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/hb_student_tracker";
+        String user = "postgres";
+        String pass = "admin";
+
+        try (Connection conn = DriverManager.getConnection(url, user, pass)) {
+            System.out.println("Connection is successful");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
