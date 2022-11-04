0452bb33f1a1a2a7fc5779e69e43f2ea55b05aa3
alihmzyv <alihmzyv@gmail.com>
2022-11-04 18:20:35 +0400

    last studies

---
+++ src/test/java/com/luv2code/jdbc/HibernateTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
